1499b59facba71bf0bdc9e0167ccf4d2e5cb9cc2
Guillermo A. Fisher <guillermoandraefisher@gmail.com>
2014-05-03 21:14:59 -0400

    BaSh stuff.

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # set a variable for the current directory
-declare -r framework_dir="$(dirname $(cd "${0%/*}"; echo $(pwd)))"
+framework_dir="$(dirname $(cd "${0%/*}"; echo $(pwd)))"
 
 # work in the correct directory
 cd ${framework_dir}
@@ -12,9 +12,9 @@ composer install --dev
 # test coding standards
 echo
 echo "Running PHP Code Sniffer..."
-vendor/bin/phpcs --standard=PSR2 --extensions=php src tests
+${framework_dir}/vendor/bin/phpcs --standard=PSR2 --extensions=php src tests
 
 # generate the code coverage report
 echo
 echo "Generating code coverage report..."
-vendor/bin/phpunit --coverage-html reports/coverage --coverage-clover clover.xml tests
+${framework_dir}vendor/bin/phpunit --coverage-html reports/coverage --coverage-clover clover.xml tests
